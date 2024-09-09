516379bda6dc003d67469938b08871a1b9fa7e40
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-09-09 16:12:35 +0800

    backing properties

--- src/main/kotlin/com/app/kotlinjava/Repository.kt
+++ src/main/kotlin/com/app/kotlinjava/Repository.kt
@@ -1,39 +1,35 @@
 package com.app.kotlinjava
 
-import java.util.*
-
 /**
  * we need only one instance of repository. to do so, we replaced the keyword 'class'
  * to 'object'
  */
 object Repository {
-    private val users = mutableListOf<User>()
+    private val _users = mutableListOf<User>()
+    val users: List<User>
+        get() = _users
 
     // keeping the constructor private to enforce the usage of getInstance
     init {
         val user1 = User("Jane", "")
         val user2 = User("John", null)
         val user3 = User("Anne", "Doe")
-        
-        users.add(user1)
-        users.add(user2)
-        users.add(user3)
-    }
 
-    fun getUsers(): List<User?> {
-        return users
+        _users.add(user1)
+        _users.add(user2)
+        _users.add(user3)
     }
 
     val formattedUserNames: List<String>
         get() {
-            return users.map { user ->
+            return _users.map { user ->
                 if (user.lastName != null) {
                     if (user.firstName != null) {
                         "${user.firstName} ${user.lastName}"
                     } else {
                         user.lastName ?: "Unknown"
                     }
-                }  else {
+                } else {
                     user.firstName ?: "Unknown"
                 }
             }
