e89a4b61fb6ebe5a686e24b47e303f5e668e8328
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-10-16 17:40:50 +0800

    function practice

--- src/main/kotlin/com/app/kotlinjava/basics/functions/FunctionsDemo.kt
+++ src/main/kotlin/com/app/kotlinjava/basics/functions/FunctionsDemo.kt
@@ -1,5 +1,43 @@
 package com.app.kotlinjava.basics.functions
 
+
+
+fun main(args: Array<String>) {
+}
+
+fun memberReference() {
+
+}
+
+class PersonTestDemo(val name: String, val age: Int)
+
+fun functionType() {
+
+    val isEven: (Int) -> Boolean = { i: Int -> i % 2 == 0 } // the return type of isEven is (Int) -> Boolean
+    println(isEven(10))
+
+    // what is the difference between these two?
+    // () -> Int? = return type is NULLABLE; not the whole type
+    // (() -> Int)? = whole type is NULLABLE
+
+    // EXAMPLE:
+
+    // WRONG val f2: () -> Int? = null # Null cannot be a value of a non-null type () -> Int?
+    val f2: () -> Int? = { null } // lambda without arguments that always return null
+    println(f2())
+
+    // working with a nullable function type
+    val f:(() -> Int)? = null
+    // how to call? by surrounding with a null checking
+    if (f != null) {
+        f()
+    }
+    // or
+    f?.invoke()
+}
+
+
+
 /**
  * function declaration starts with the
  * 1. fun keyword
